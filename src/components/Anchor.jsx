88935da91d6230d8660b7f3140a97e379d50f276
giperpasha <giperpasha1992@gmail.com>
2017-11-05 23:26:33 +0200

    Updates for 05.11

--- src/components/Anchor.jsx
+++ src/components/Anchor.jsx
@@ -2,20 +2,41 @@ import React, {Component} from 'react';
 import Fontawesome from "react-fontawesome";
 
 class Anchor extends Component {
-  handleScroll(){
+  constructor(props) {
+      super(props);
+
+      this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  handleVisibility(){
     this.anchor.style.opacity = document.documentElement.scrollTop / 500 || document.body.scrollTop / 500;
   };
 
+  handleScroll() {
+      let scrollID = setInterval(() => {
+          document.documentElement.scrollTop -= 15;
+          document.body.scrollTop -= 15;
+
+          console.log(document.documentElement.scrollTop);
+          console.log(document.body.scrollTop);
+          console.log(window.pageYOffset);
+
+          if(!window.pageYOffset) {
+              clearInterval(scrollID);
+          }
+      }, 1)
+  }
+
   componentDidMount() {
-      window.addEventListener("scroll", this.handleScroll.bind(this))
+      window.addEventListener("scroll", this.handleVisibility.bind(this))
   }
 
   render() {
     return (
-      <div className="anchor" ref={(div) => { this.anchor = div }}>
-        <a href="#top" className="anchor-link">
+      <div className="anchor" ref={(div) => { this.anchor = div }} onClick={this.handleScroll}>
+        <div className="anchor-link">
           <Fontawesome name="angle-up" size="3x"/>
-        </a>
+        </div>
       </div>
     )
   }
