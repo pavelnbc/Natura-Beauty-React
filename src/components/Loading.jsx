2daf36966e5cd4cce896626a26b02f4f23dab0fd
pavelnbc <giperpasha@rambler.ru>
2018-01-02 22:02:55 +0200

    Updates for 02.01

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Fontawesome from 'react-fontawesome';
+
+function Loading() {
+    return (
+        <div className="loading">
+            <img src="/img/loading.gif" alt=""/>
+        </div>
+    )
+}
+
+export default Loading
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,44 +1,40 @@
-import React, {Component} from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Grid, Row, Col } from "react-bootstrap";
 
-import Product from '../components/Product';
-import EmptyPage from '../components/EmptyPage';
+import Product from "../components/Product";
+import EmptyPage from "../components/EmptyPage";
+import Loading from '../components/Loading';
 
 function Products({ products, match, searchedMed, getProduct }) {
+    let medications = match.params.category
+        ? products.filter(medication => {
+            return medication.category === match.params.category;
+        })
+        : products;
 
-  let medications = match.params.category
-    ? products.filter((medication) => {
-      return medication.category === match.params.category
-    })
-    : products;
+    let searchMeds = medications.filter(med => {
+        if (~med.slug.indexOf(searchedMed.toLocaleLowerCase())) {
+            return med;
+        }
+    });
 
-  let searchMeds = medications.filter((med) => {
-    if (~med.slug.indexOf(searchedMed.toLocaleLowerCase())) {
-      return med
-    }
-  });
-
-  return (
-      (!medications.length && !searchMeds.length)
-      ? <EmptyPage content="Loading..." /> :
-      (!searchMeds.length)
-      ? <EmptyPage content="Nothing was found" />
-      : <Grid>
-        <Row className="show-grid">
-          {searchMeds.map(((product, index) => {
-            return (
-              <Col xs={12} sm={6} md={4} lg={4} key={index}>
-                <Product key={index}
-                  medication={product}
-                  getItem={getProduct}
-                />
-              </Col>
-            )
-          }))}
-        </Row>
-      </Grid>
-  )
+    return !medications.length && !searchMeds.length ? (
+        <Loading />
+    ) : !searchMeds.length ? (
+        <EmptyPage content="Nothing was found" />
+    ) : (
+        <Grid>
+            <Row className="show-grid">
+                {searchMeds.map((product, index) => {
+                    return (
+                        <Col xs={12} sm={6} md={4} lg={4} key={index}>
+                            <Product key={index} medication={product} getItem={getProduct} />
+                        </Col>
+                    );
+                })}
+            </Row>
+        </Grid>
+    );
 }
 
-
-export default Products
+export default Products;
