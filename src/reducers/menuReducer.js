0f128422a40b36de506e82e5f93ad3640440d3b0
pavelnbc <giperpasha@rambler.ru>
2018-02-14 11:47:04 +0200

    Updates for 14.02

--- src/reducers/menuReducer.js
+++ src/reducers/menuReducer.js
@@ -1,11 +1,17 @@
 import { HANDLE_MENU, OFF_MENU } from '../actions';
 
 export function menuReducer(state = false, action) {
+  let bodyStyle = document.body.style;
+
   switch(action.type) {
-    case OFF_MENU: 
+    case OFF_MENU:
+      bodyStyle.overflow = '';
+
       return false;
 
     case HANDLE_MENU:
+       bodyStyle.overflow = 'hidden';
+
        return true;
 
     default: 
