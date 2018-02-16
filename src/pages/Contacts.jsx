45594f99947a3479fc9e6e6a4fd0db206515fd9e
pavelnbc <giperpasha@rambler.ru>
2018-02-16 09:35:39 +0200

    Updates for 16.02

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -12,23 +12,21 @@ function Contacts() {
             You may contact us any time it is conveniently for you - we work <b> 24/7</b>.
           </p>
 
-          <div className="contacts-field">
-            <div>Toll free number: </div>
-            <div>1-800-476-30-50</div>
-          </div>
+          <iframe title="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2396.381494016536!2d8.832481315422275!3d53.085383201927534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b127de36b5241d%3A0xa5a06acc85e6cfcb!2zTWF4LVJlZ2VyLVN0cmHDn2UgMSwgMjgyMDkgQnJlbWVuLCDQndGW0LzQtdGH0YfQuNC90LA!5e0!3m2!1suk!2sua!4v1508925525301" allowfullscreen></iframe>
 
-          <div className="contacts-field">
-            <div>Email:</div>
-            <div>the_best_pharmacy@yahoo.com</div>
-          </div>
-
-          <div className="location">
-            <div className="contacts-field">
-              <div>Physical address:</div>
-              <div>Max-Reger-Straße 1, 28209 Bremen, Germany</div>
+          <section className="contacts-field">
+            <div>
+                <div>Toll free number: </div>
+                <div>Email:</div>
+                <div>Physical address:</div>
             </div>
-            <iframe title="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2396.381494016536!2d8.832481315422275!3d53.085383201927534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b127de36b5241d%3A0xa5a06acc85e6cfcb!2zTWF4LVJlZ2VyLVN0cmHDn2UgMSwgMjgyMDkgQnJlbWVuLCDQndGW0LzQtdGH0YfQuNC90LA!5e0!3m2!1suk!2sua!4v1508925525301" width="600" height="450" frameborder="0" allowfullscreen></iframe>
-          </div>
+
+            <div>
+                <div>1-800-476-30-50</div>
+                <div>the_best_pharmacy@yahoo.com</div>
+                <div>Max-Reger-Straße 1, 28209 Bremen, Germany</div>
+            </div>
+          </section>
 
         </section>
     )
