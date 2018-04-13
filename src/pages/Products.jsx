68cea198f992255710c19cfb68a0d0f2c11bc4f0
pavelnbc <giperpasha@rambler.ru>
2018-04-13 15:51:06 +0300

    Updates for 13.04

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,84 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Product from '../components/Product';
+import Loading from '../components/Loading'
+import EmptyPage from '../components/EmptyPage';
+
+function Products ({ products, searchedMed, match, getProduct }) {
+    const medications = match.params.category
+        ? products.filter((med) => {
+            return med.category === match.params.category
+        })
+        : products;
+
+    const searchedMeds = medications.filter((med) => {
+        if(~med.slug.indexOf(searchedMed.toLowerCase())) {
+            return med
+        }
+    });
+
+    console.log(products);
+
+    return (
+        !products.length
+        ? <Loading/>
+        : !searchedMeds.length
+        ? <EmptyPage content="Nothing was found"/>
+        : <div className="row">
+                {searchedMeds.map((med, index) => {
+                    return (
+                        <div key={index} className="col-xs-12 col-sm-4 col-md-6 col-lg-3">
+                            <Product medication={med} getItem={getProduct}/>
+                        </div>
+                    )
+                })}
+            </div>
+    )
+}
+
+Products.propTypes = {
+    products: PropTypes.array,
+    searchedMed: PropTypes.string,
+    match: PropTypes.object,
+    getItem: PropTypes.func
+};
+
+export default Products
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React from "react";
 import PropTypes from 'prop-types';
 
 import Product from "../components/Product";
@@ -45,4 +125,4 @@ Products.propTypes = {
   match: PropTypes.object
 };
 
-export default Products;
+export default Products;*/
